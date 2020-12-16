330b3f6e95e997d8963aa5a22b0763f5dc6a9ff7
scheehan <siowcheehan@gmail.com>
2020-12-16 11:10:10 +0800

    clean up

--- cgi/decode_64.py
+++ cgi/decode_64.py
@@ -3,16 +3,12 @@
 import base64
 import cgi
 
+form = cgi.FieldStorage() 
 
-# form = cgi.FieldStorage() 
-
-# d_data = form.getvalue('d_box')
-
-d_data = 'null'
-
-#print(d_data)
+d_data = form.getvalue('d_box')
 
 dec_data = base64.b64decode(d_data + "=" * (-len(d_data) % 4))
+
 
 print ("Content-type: text/html\n")
 
@@ -21,28 +17,13 @@ print("""\
     
     <head>
 <title>Sample CGI Script</title>
-<link rel="stylesheet" href="../css/main.css">
+<link rel="stylesheet" href="css/main.css">
 </head>
 
 """)
 print('<body>\n')
 
-print(dec_data)
+print('<p>' + dec_data.decode("utf-8") + '</p>')
 
-
-print("""\
-    
-<form name="e_box" action="cgi-bin/encode_64.py" method="POST">
-<input type="text" id="e_box" name="e_box"><br>
-<input type="submit" id="e_box" value="Submit">
-</form>')
-
-<form name="d_box" action="cgi-bin/decode_64.py" method="POST">
-<input type="text" id="d_box" name="d_box"><br>
-<input type="submit" id="d_box" value="Submit">
-</form>
-
-""")
-  
 print('</body>\n')
 print('</html>\n')
--- cgi/encode_64.py
+++ cgi/encode_64.py
@@ -7,34 +7,25 @@ form = cgi.FieldStorage()
 
 f_data = form.getvalue('e_box')
 
-en_data = base64.b64encode(b'f_data)
+en_data = base64.b64encode(f_data.encode('ascii'))
 
 print ("Content-type: text/html\n")
 
 print ('<html lang="en">\n')
+
 print("""\
     
     <head>
 <title>Sample CGI Script</title>
-<link rel="stylesheet" href="../css/main.css">
+<link rel="stylesheet" href="css/main.css">
 </head>
 
 """)
+
 print('<body>\n')
 
-print("""\
-    
-<form name="e_box" action="cgi-bin/encode_64.py" method="POST">
-<input type="text" id="e_box" name="e_box"><br>
-<input type="submit" id="e_box" value="Submit">
-</form>')
+print('<p>' + en_data.decode("utf-8") + '</p>')
 
-<form name="d_box" action="cgi-bin/decode_64.py" method="POST">
-<input type="text" id="d_box" name="d_box"><br>
-<input type="submit" id="d_box" value="Submit">
-</form>
 
-""")
-  
 print('</body>\n')
 print('</html>\n')
