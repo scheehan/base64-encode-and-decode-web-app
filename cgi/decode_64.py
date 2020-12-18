c7ae4f7f336ae9c492ff24d1122d651f02cbfe8e
scheehan <siowcheehan@gmail.com>
2020-12-18 22:58:52 +0800

    fix bug - empty data (nonetype) checks

--- cgi/decode_64.py
+++ cgi/decode_64.py
@@ -3,19 +3,23 @@
 import base64
 import cgi
 
-form = cgi.FieldStorage()       #create instance of FieldStorage
+form = cgi.FieldStorage()                                                       #create instance of FieldStorage
 
-d_data = form.getvalue('d_box') #get submit data from form name d_box
+d_data = form.getvalue('d_box')                                                 #get submit data from form name d_box
 
-dec_data = base64.b64decode(d_data + "=" * (-len(d_data) % 4))
-
-
-print ("Content-type: text/html\n")
-print ('<html lang="en">\n')
-
-print('<body>\n')
-
-print('<p>' + dec_data.decode("utf-8") + '</p>')
-
-print('</body>\n')
-print('</html>\n')
+if d_data is None:                                                              #check for nonetype
+    print ("Content-type: text/html\n")
+    print ("<html>\n")
+    print("<body>\n")
+    print ("Please fill in value")
+    print("</body>\n")
+    print("</html>\n")
+    exit
+else:
+    dec_data = base64.b64decode(d_data + "=" * (-len(d_data) % 4))              #add padding to bytes before decode
+    print ("Content-type: text/html\n")
+    print ('<html>\n')
+    print('<body>\n')
+    print('<p>' + dec_data.decode("utf-8") + '</p>')
+    print('</body>\n')
+    print('</html>\n')
--- cgi/encode_64.py
+++ cgi/encode_64.py
@@ -3,19 +3,22 @@
 import base64
 import cgi
 
-form = cgi.FieldStorage()                                   #create instance of FieldStorage
+form = cgi.FieldStorage()                                       #create instance of FieldStorage
+f_data = form.getvalue('e_box')                                 #get submit data from form name e_box
 
-f_data = form.getvalue('e_box')                             #get submit data from form name e_box
-
-en_data = base64.b64encode(f_data.encode('ascii'))          #encode data as ascii format, then encode with base 64
-
-# return data in HTTP 
-print ("Content-type: text/html\n")
-
-print ('<html lang="en">\n')
-print('<body>\n')
-
-print('<p>' + en_data.decode("utf-8") + '</p>')
-
-print('</body>\n')
-print('</html>\n')
+if f_data is None:                                              #check for nonetype
+    print ("Content-type: text/html\n")
+    print ("<html>\n")
+    print("<body>\n")
+    print ("Please fill in value")
+    print("</body>\n")
+    print("</html>\n")
+    exit
+else:
+    en_data = base64.b64encode(f_data.encode('ascii'))          #encode data as ascii format, then encode with base64
+    print ("Content-type: text/html\n")                         #return data in HTTP
+    print ("<html>\n")
+    print("<body>\n")
+    print('<p>' + en_data.decode("utf-8") + '</p>')
+    print("</body>\n")
+    print("</html>\n")
